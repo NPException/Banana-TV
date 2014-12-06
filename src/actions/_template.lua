abd100ced7c85de2ee7ba1d4cb08cd3331dc6d1d
ChaosChaot <chaoschaot86@gmail.com>
2014-12-06 13:47:32 +0100

    change template.lua to _template.lua

---
+++ src/actions/_template.lua
@@ -0,0 +1,18 @@
+-- --------------------------
+-- Action: 
+-- --------------------------
+return {
+  name = "",
+  animation = nil,  -- Filepath
+  icon = love.graphics.newImage("assets/actions/placeholder.txt"),
+  score = {
+    baby    = { scare = 0, boring = 0, delight = 0},
+    girl    = { scare = 0, boring = 0, delight = 0},
+    boy     = { scare = 0, boring = 0, delight = 0},
+    emo     = { scare = 0, boring = 0, delight = 0},
+    dad     = { scare = 0, boring = 0, delight = 0},
+    mom     = { scare = 0, boring = 0, delight = 0},
+    grandma = { scare = 0, boring = 0, delight = 0},
+    grandpa = { scare = 0, boring = 0, delight = 0}
+    }
+  }
