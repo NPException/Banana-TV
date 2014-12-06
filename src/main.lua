9b403f3406644618a238b3791cea5385e984b667
NPException <npexception42@gmail.com>
2014-12-06 15:35:35 +0100

    center draw area on rescale

--- src/main.lua
+++ src/main.lua
@@ -1,31 +1,69 @@
 require("lib.stringfunctions")
 require("lib.requirefolder")
 
+GLOBALS = {}
+local globals = GLOBALS
+
+local game
+
 -- LOAD --
 function love.load(arg)
+  globals.config = require("conf")
+  globals.scale = globals.config.resY / love.window.getHeight()
+  globals.offX = 0
+  globals.offY = 0
   
-  local items = requirefolder("items")
-  print()
-  print("Items: ")
-  for k,v in pairs(items) do
-    print(k.." -> "..v.name)
-  end
+  love.graphics.setDefaultFilter("nearest","nearest")
+  
+  game = require("game.Game").new()
 end
 
 -- KEYPRESSED --
 function love.keypressed(key)
+  game:keypressed(key)
+  
+  if (key == "return") then
+    if (love.keyboard.isDown("lalt") or love.keyboard.isDown("ralt")) then
+      love.window.setFullscreen(not love.window.getFullscreen(), "desktop")
+    end
+  end
 end
 
 function love.mousepressed( x, y, button )
+  game:mousepressed(x,y,button)
 end
 
 -- UPDATE --
 function love.update(dt)
+  game:update(dt)
 end
 
 -- DRAW --
+local lg = love.graphics
 function love.draw()
+  lg.push()
+  lg.scale(1/globals.scale, 1/globals.scale)
+  lg.translate(-globals.offX, -globals.offY)
+  
+    game:draw()
+    
+  lg.pop()
 end
 
 function love.resize(w,h)
+  local sx = globals.config.resX / w
+  local sy = globals.config.resY / h
+  
+  if sx < sy then
+    globals.offX = (globals.config.resX - w*sy)/2
+    globals.offY = 0
+  elseif sy < sx then
+    globals.offX = 0
+    globals.offY = (globals.config.resY - h*sx)/2
+  else
+    globals.offX = 0
+    globals.offY = 0
+  end
+  
+  globals.scale = math.max(sx,sy)
 end
