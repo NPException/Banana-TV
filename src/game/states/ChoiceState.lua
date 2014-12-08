fa93a16802df93eba7fc84a1403dade1706202b0
NPException <npexception42@gmail.com>
2014-12-08 22:10:20 +0100

    fixed choiceState again ;D

--- src/game/states/ChoiceState.lua
+++ src/game/states/ChoiceState.lua
@@ -9,16 +9,14 @@ function Choice.new(game)
   choice.game = game
   choice.started = true
   
-  choice.actions = {
-      -- { action=_THEACTION_, rect={x=0,y=0,w=128,h=128} }
-  }
+  choice.actions = {}
+  
+  local size = 128
   
   local partX = globals.config.resX/7
   local partY = globals.config.resY/4
-  local imgX = partX
-  local imgY = partY
-  
-  local size = 128
+  local imgX = partX - size/2
+  local imgY = partY - size/2
   
   for _, action in ipairs(choice.game.actions) do
     
@@ -26,7 +24,7 @@ function Choice.new(game)
     
     imgX = imgX+partX
     if  imgX > globals.config.resX - partX then
-      imgX = partX
+      imgX = partX - size/2
       imgY = imgY + partY
     end
   end
@@ -51,16 +49,12 @@ function Choice:drawGUI()
     
     local action = entry.action
     local rect = entry.rect
-    local imgX = entry.rect.x
-    local imgY = entry.rect.y
-    
-    local size = entry.rect.w -- or set global size
     
     lg.setColor(0,0,0,200)
-    lg.rectangle("fill", imgX-size/2, imgY-size/2, size, size)
+    lg.rectangle("fill", rect.x, rect.y, rect.w, rect.h)
     lg.setColor(255,255,255)
-    lg.draw(action.icon,imgX,imgY, 0, size/action.icon:getWidth(), size/action.icon:getHeight(),
-            action.icon:getWidth()/2,action.icon:getHeight()/2)
+    lg.draw(action.icon, rect.x, rect.y, 0,
+            rect.w/action.icon:getWidth(), rect.h/action.icon:getHeight())
 
   end
 end
